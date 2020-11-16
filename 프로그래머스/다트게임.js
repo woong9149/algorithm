f9a3bea28fb59074d10df34333276352a9adba74
woong9149 <sewoong0409@naver.com>
2020-11-17 01:27:04 +0900

    다트게임_ing

--- 프로그래머스/다트게임.js
+++ 프로그래머스/다트게임.js
@@ -1,16 +1,32 @@
 function solution(dartResult) {
     var answer = 0;
-    let copyArr = Array.from(dartResult);
-    dartResult.forEach(function (arg){
+    const reducer = (accumulator, currentVal) => accumulator + currentVal;
+    let copyArr = Array.from(dartResult.split(''));
+    dartResult.split('').forEach(function (arg){
+        let index = copyArr.indexOf(arg);
+        // console.log(dartResult[index - 1]);
         switch (arg){
             case 'S':
-                copyArr.splice(dartResult[dartResult.indexOf(arg) - 1],dartResult[dartResult.indexOf(arg) + 1],parseInt(dartResult[dartResult.indexOf(arg) - 1]) * 1) ;
+                copyArr.splice((index - 1),2,(parseInt(copyArr[index - 1]) * 1)) ;
+                break;
             case 'D':
-                return dartResult[dartResult.indexOf(arg) - 1] * dartResult[dartResult.indexOf(arg) - 1] ;
+                copyArr.splice((index - 1),2,(parseInt(copyArr[index - 1]) * parseInt(copyArr[index - 1]))) ;
+                break;
             case 'T':
-                return dartResult[dartResult.indexOf(arg) - 1] * dartResult[dartResult.indexOf(arg) - 1] * dartResult[dartResult.indexOf(arg) - 1];
+                copyArr.splice((index - 1),2,(parseInt(copyArr[index - 1]) * parseInt(copyArr[index - 1]) * parseInt(copyArr[index - 1]))) ;
+                break;
+            case '#':
+                copyArr.splice((index - 1),2,(parseInt(copyArr[index - 1]) * (-1)));
+                break;
+            case '*':
+                copyArr.splice((index - 2),1,(parseInt(copyArr[index - 2]) * 2));
+                copyArr.splice((index - 1),2,(parseInt(copyArr[index - 1]) * 2));
+                break;
         }
+
     })
+    console.log(copyArr);
+    answer = copyArr.reduce(reducer,0);
     return answer;
 }
 
