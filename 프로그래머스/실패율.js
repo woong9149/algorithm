d49d1d978c515bf228e7d4b6d047bd61962e66f7
woong9149 <sewoong0409@naver.com>
2020-11-20 23:23:53 +0900

    다트게임 수정

--- 프로그래머스/실패율.js
+++ 프로그래머스/실패율.js
@@ -1,21 +1,40 @@
-function solution(N,stages) {
+// function solution(N,stages) {
+// 	var answer = [];
+// 	var	result = [];
+
+// 	for(let i = 0; i < N; i++) {
+// 			var a = stages.filter(arg => arg >= i+1).length;
+// 			var b = stages.filter(arg => arg === i+1).length;
+// 			if( b === 0){
+// 					result.push(0);
+// 			}else {
+// 			result.push(b/a);
+// 			}
+// 	}
+
+// 	for(var i =0; i < N; i++) {
+// 			var index = result.indexOf(Math.max.apply(null, result));
+// 			answer.push(index+1); //answer[i] = index+1;도 가능
+// 			result[index] = -1;
+// 	}
+// 	return answer;
+// }
+function solution(N, stages) {
 	var answer = [];
-	var	result = [];
-
-	for(let i = 0; i < N; i++) {
-			var a = stages.filter(arg => arg >= i+1).length;
-			var b = stages.filter(arg => arg === i+1).length;
-			if( b === 0){
-					result.push(0);
-			}else {
-			result.push(b/a);
-			}
+	let person = stages.length;
+	var resultArr =[];
+	
+	for(let i = 0; i < N.length; i++){
+		let cnt = 0; 
+		let failRate = 0;
+		cnt += (stages.filter(arg => arg === N[i]) ? 1 : 0);
+		failRate = cnt / person;
+		resultArr.push({'num' : i , 'failRate' : failRate});
 	}
 
-	for(var i =0; i < N; i++) {
-			var index = result.indexOf(Math.max.apply(null, result));
-			answer.push(index+1); //answer[i] = index+1;도 가능
-			result[index] = -1;
-	}
+
 	return answer;
 }
+
+
+
