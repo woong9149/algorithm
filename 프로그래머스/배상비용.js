4f599a9ee7840fe895fd482a407cc07900c1f3f8
woong9149 <sewoong0409@naver.com>
2020-12-03 23:27:47 +0900

    [프로그래머스]Lv.2

--- 프로그래머스/배상비용.js
+++ 프로그래머스/배상비용.js
@@ -1,22 +1,27 @@
 function solution(no, works) {
 	var result = 0;
-	works = works.sort();
 
-	while(no > 0){
-		let maxNum = Math.max.apply(null, works);
-		if(maxNum > 0){
-			let idx = works.indexOf(maxNum);
-			works[idx] = maxNum - 1;
-			no -= 1;
-		} else {
-            break;
-        }
+	let workSum = works.reduce((accu, curr) => accu + curr);
+  if (workSum > no) {
+		while(no > 0){
+			let maxNum = Math.max.apply(null, works);
+			if(maxNum > 0){
+				let idx = works.indexOf(maxNum);
+				works[idx] = maxNum - 1;
+				// works.splice(idx,1,maxNum - 1);
+				no -= 1;
+			} else {
+							return 0;
+					}
+		}
+		works.forEach(element => {
+			element *= element;
+			result = result + element;
+		});
+		return result;
+	} else {
+		return 0;
 	}
-	works.forEach(element => {
-		element *= element;
-		result = result + element;
-	});
-	return result;
 }
 
 solution(4, [4,3,4]);
--- 프로그래머스/짝지어제거하기.js
+++ 프로그래머스/짝지어제거하기.js
@@ -1,10 +1,15 @@
 function solution(s)
 {
     var answer = 0;
-		var sArr = s.split('');
-    for(let i = 0; i < sArr.length; i++){
-
-		}
-
-    return answer;
+    var sArr = [];
+    
+    for (let i = 0; i < s.length; i++) {
+      if( (sArr.length === 0 ) || (sArr[sArr.length-1] !== s[i])) {
+        sArr.push(s[i]);
+      } else {
+        sArr.pop();
+      }
+    }
+  
+    return answer = (sArr.length === 0) ? 1 : 0;
 }
