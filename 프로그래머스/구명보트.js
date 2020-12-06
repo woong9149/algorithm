103ffc0baf6d1c90e062f149e59c7e6c138a860d
woong9149 <sewoong0409@naver.com>
2020-12-06 23:57:56 +0900

    [프로그래머스]Lv.2 구명보트

--- 프로그래머스/구명보트.js
+++ 프로그래머스/구명보트.js
@@ -1,20 +1,22 @@
 function solution(people, limit) {
-	var answer = 0;
+	let arr = people.filter(value => value < limit);
+	let answer = people.length - arr.length;
+	let arrLength = arr.length;
 	
-	const copyPeople = Array.from(people);
-	const calc = (curr, next) => curr + next;
-	for (let i = 0; i < copyPeople.length; i++) {
-		let temp;
-		for(let j = i + 1; j < copyPeople.length; j++) {
-			 answer = copyPeople.slice(i + 1, j + 1).reduce(calc, copyPeople[i]) <= limit ? answer++ : answer;
-		}
+	arr.sort((a,b) => b-a);
+	while (arrLength > 0){
+			const number = arr.pop();
+			const idx = arr.findIndex(value => value + number <= limit);
+			
+			if (idx !== -1) {
+					arr.splice(0, idx + 1);
+					answer += idx + 1;
+			} else {
+					answer += len + 1;
+					break;
+			}
 	}
-	console.log(answer);
 	return answer;
-};
+}
 
 solution([70, 50, 80, 50], 100);
-/**
- *  point. 반복문을 돌 때 마다, 모든 사람을 구출할 수 있는 경우의 수를 만들어야함.===>이게 관건이 아닌가
- * 					그 때 필요한 보트의 갯수를 비교해서 가장 작은 수를 반환해야함.
- */
