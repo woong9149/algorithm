65429af58f4c00f3c2e14562ccffd46278b18dca
woong9149 <sewoong0409@naver.com>
2020-12-07 01:45:00 +0900

    [프로그래머스]Lv.2 구명보트

--- 프로그래머스/구명보트.js
+++ 프로그래머스/구명보트.js
@@ -1,20 +1,14 @@
 function solution(people, limit) {
-	let arr = people.filter(value => value < limit);
-	let answer = people.length - arr.length;
-	let arrLength = arr.length;
-	
-	arr.sort((a,b) => b-a);
-	while (arrLength > 0){
-			const number = arr.pop();
-			const idx = arr.findIndex(value => value + number <= limit);
-			
-			if (idx !== -1) {
-					arr.splice(0, idx + 1);
-					answer += idx + 1;
-			} else {
-					answer += len + 1;
-					break;
-			}
+	let answer = 0;
+	let peopleLength = people.length;
+	people.sort((a,b) => b-a); //people 배열을 내림 차순으로 정렬
+
+	for ( let i = 0 ; i < peopleLength; i++) {
+		//제일 무거운사람과 제일 가벼운 사람의 합이 무게제한보다 작거나 같으면 peopleLength -1
+		if (people[i] + people[peopleLength - 1] <= limit ) {
+			peopleLength--;
+		}
+		answer++;
 	}
 	return answer;
 }
