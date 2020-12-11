ac7fe51e667c9d3c804b7c4ba82f98b92079687d
woong9149 <sewoong0409@naver.com>
2020-12-11 23:59:07 +0900

    [프로그래머스]Lv2. 조이스틱

---
+++ 프로그래머스/조이스틱.js
@@ -0,0 +1,11 @@
+function solution(name) {
+	var answer = 0;
+
+	const nameToAscArr = name.split('').map((arg) => arg.charCodeAt());
+	const cntStick = nameToAscArr.map((num) => { 
+	
+	});
+	
+
+	return answer;
+}
